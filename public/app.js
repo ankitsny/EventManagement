bf7b25b6432959baa02a36edc027c6b24079eb13
Ankit Kumar <ankit@unobridge.com>
2017-07-08 19:01:59 +0530

    added angular-route

--- public/app.js
+++ public/app.js
@@ -1,14 +1,18 @@
 var app = angular.module('innovify', [
+    'ngRoute',
     'app.controllers',
     'app.services',
     'app.directives'
 ]);
 
 app.config(['$routeProvider', '$locationProvider',
+
     function ($routeProvider, $locationProvider) {
+
         $locationProvider.html5Mode(true);
 
         $routeProvider
+
             .when('/', {
                 template: '<h1>Hello World</h1>'
             })
